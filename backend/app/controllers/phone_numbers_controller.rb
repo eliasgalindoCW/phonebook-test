15af1e0bd260d74f9bf34d01c5867bd364405c68
eliasgalindoCW <elias.galindo@cloudwalk.io>
2024-07-12 00:35:12 -0300

    Feat: phone numbers controller and respective route

--- backend/app/controllers/phone_numbers_controller.rb
+++ backend/app/controllers/phone_numbers_controller.rb
@@ -1,2 +1,41 @@
 class PhoneNumbersController < ApplicationController
-end
+    before_action :set_phone_number, only: [:show, :update, :destroy]
+  
+    def index
+      @phone_numbers = PhoneNumber.all
+      render json: @phone_numbers
+    end
+  
+    def create
+      @phone_number = PhoneNumber.new(phone_number_params)
+  
+      if @phone_number.save
+        render json: @phone_number, status: :created
+      else
+        render json: @phone_number.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @phone_number.update(phone_number_params)
+        render json: @phone_number
+      else
+        render json: @phone_number.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @phone_number.destroy
+    end
+  
+    private
+  
+    def set_phone_number
+      @phone_number = PhoneNumber.find(params[:id])
+    end
+  
+    def phone_number_params
+      params.require(:phone_number).permit(:name, :phone_number, :notes)
+    end
+  end
+  
