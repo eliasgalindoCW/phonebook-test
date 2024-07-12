3b555beb80b9c835e720e2cd2bf1b695367fbbdf
eliasgalindoCW <elias.galindo@cloudwalk.io>
2024-07-12 14:26:36 -0300

    Fix: Routes

--- backend/app/controllers/phone_numbers_controller.rb
+++ backend/app/controllers/phone_numbers_controller.rb
@@ -1,11 +1,18 @@
 class PhoneNumbersController < ApplicationController
     before_action :set_phone_number, only: [:show, :update, :destroy]
   
+    # GET /phone_numbers
     def index
       @phone_numbers = PhoneNumber.all
       render json: @phone_numbers
     end
+
+    # GET /phone_numbers/ïd
+    def show
+      render json: @phone_number
+    end
   
+    # POST /phone_numbers
     def create
       @phone_number = PhoneNumber.new(phone_number_params)
   
@@ -16,6 +23,7 @@ class PhoneNumbersController < ApplicationController
       end
     end
   
+    # PATCH /phone_numbers/:id
     def update
       if @phone_number.update(phone_number_params)
         render json: @phone_number
@@ -24,6 +32,7 @@ class PhoneNumbersController < ApplicationController
       end
     end
   
+    # DELETE /phone_numbers/:id
     def destroy
       @phone_number.destroy
     end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  Rails.application.routes.draw do
-    resources :phone_numbers
-  end
   
-
+  resources :phone_numbers
+  
   # Defines the root path route ("/")
   # root "posts#index"
 end
