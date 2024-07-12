aa6abb45cd0034a0c6c9b3dc0ec66e5c186ae710
eliasgalindoCW <elias.galindo@cloudwalk.io>
2024-07-12 00:25:58 -0300

    First commit -Backend Dev Enviroment

---
+++ db/migrate/20240712032442_create_phone_numbers.rb
@@ -0,0 +1,11 @@
+class CreatePhoneNumbers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :phone_numbers do |t|
+      t.string :name
+      t.string :phone_number
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
