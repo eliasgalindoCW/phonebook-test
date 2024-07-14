b75fac5f1f3e4a5b12447b9239dcffa0cd67e178
EliasGalindo0 <eliasgalindo@gmail.com>
2024-07-14 11:56:07 -0300

    Feat: Header

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,12 @@
 import Book from "@/components/Book";
 import Dial from "@/components/Dial";
 import Form from "@/components/Form";
-import Header from "@/components/Header";
 import History from "@/components/History";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <section>
+      <section className="m-1 p-1">
         <Form />
         <Dial />
         <Book />
--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,7 +1,28 @@
 export default function Header() {
-    return(
-        <>
-            <h1>Lista Telefônica</h1>
-        </>
-    )
+  return (
+    <header className="bg-purple-950 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">Phonebook App</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/" className="hover:text-gray-300">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/add" className="hover:text-gray-300">
+                Add Contact
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="hover:text-gray-300">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
