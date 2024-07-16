edd7ff2375f54357f0b3a3e190a8dcf66c51e179
EliasGalindo0 <eliasgalindo@gmail.com>
2024-07-16 00:08:33 -0300

    Chore: Change public folder and icon

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <header className="bg-purple-950 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Phonebook App</h1>
+        <h1 className="text-xl font-bold flex">
+          Phonebook App
+          <Image
+            src={"/icon-phbook.png"}
+            width={30}
+            height={30}
+            alt="phone-book"
+            className="ml-2"
+          />
+        </h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
