0df61b245fd7ff3d9b038159d56b49a2c2f090bd
EliasGalindo0 <eliasgalindo@gmail.com>
2024-07-16 22:46:42 -0300

    Feat: No contacts handle

--- frontend/app/components/Book.tsx
+++ frontend/app/components/Book.tsx
@@ -9,16 +9,15 @@ const Book = () => {
   const [note, setNote] = useState("");
   const [updatedContacts, setUpdatedContacts] = useState<Contacts[]>([]);
   const [open, setOpen] = useState(false);
-  
+
   useEffect(() => {
     const getContacts = async () => {
       const result = await getPhones();
       setUpdatedContacts(result);
-    }
+    };
 
     getContacts();
-  }, [])
-  
+  }, []);
 
   const handleAddNote = (contactId: number) => {
     const newContacts = updatedContacts.map((contact: any) => {
@@ -36,94 +35,104 @@ const Book = () => {
 
   return (
     <div className="w-3/4 p-4 bg-white shadow-2xl rounded-lg flex flex-col">
-      <h2 className="text-2xl font-bold mb-4">Last 5 Contacts</h2>
-      <ul className="mb-6">
-        {lastFiveContacts.map((contact) => (
-          <li key={contact.id} className="border-b py-2">
-            <div className="flex justify-between items-center">
-              <div>
-                <p>
-                  <b>{contact.name}</b>
-                </p>
-                <p>{contact.phone_number}</p>
-              </div>
-              <button
-                onClick={() => setSelectedContact(contact.id)}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-              >
-                Add Note
-              </button>
-            </div>
-            {selectedContact === contact.id && (
-              <div className="mt-2">
-                <input
-                  type="text"
-                  value={note}
-                  onChange={(e) => setNote(e.target.value)}
-                  className="w-full px-3 py-2 border rounded mb-2"
-                  placeholder="Enter note"
-                />
-                <button
-                  onClick={() => handleAddNote(contact.id)}
-                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
-                >
-                  Save Note
-                </button>
-              </div>
-            )}
-            {contact.notes && (
-              <p className="mt-2 text-gray-600">{contact.notes}</p>
-            )}
-          </li>
-        ))}
-      </ul>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-1 rounded text-center"
-        onClick={() => setOpen(!open)}
-      >
-        {open ? "Hide All Contacts" : "Show All Contacts"}
-      </button>
-      {open && (
-        <ul>
-          {updatedContacts.map((contact) => (
-            <li key={contact.id} className="border-b py-2">
-              <div className="flex justify-between items-center">
-                <div>
-                  <p>
-                    <b>{contact.name}</b>
-                  </p>
-                  <p>{contact.phone_number}</p>
-                </div>
-                <button
-                  onClick={() => setSelectedContact(contact.id)}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-                >
-                  Add Note
-                </button>
-              </div>
-              {selectedContact === contact.id && (
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    value={note}
-                    onChange={(e) => setNote(e.target.value)}
-                    className="w-full px-3 py-2 border rounded mb-2"
-                    placeholder="Enter note"
-                  />
+      {!updatedContacts.length ? (
+        <h2 className="text-2xl font-bold mb-4 text-center m-2">
+          No Contactcs Where Found - Please Add One
+        </h2>
+      ) : (
+        <>
+          <h2 className="text-2xl font-bold mb-4 text-center">
+            Last 5 Contacts
+          </h2>
+          <ul className="mb-6">
+            {lastFiveContacts.map((contact) => (
+              <li key={contact.id} className="border-b py-2">
+                <div className="flex justify-between items-center">
+                  <div>
+                    <p>
+                      <b>{contact.name}</b>
+                    </p>
+                    <p>{contact.phone_number}</p>
+                  </div>
                   <button
-                    onClick={() => handleAddNote(contact.id)}
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
+                    onClick={() => setSelectedContact(contact.id)}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
                   >
-                    Save Note
+                    Add Note
                   </button>
                 </div>
-              )}
-              {contact.notes && (
-                <p className="mt-2 text-gray-600">{contact.notes}</p>
-              )}
-            </li>
-          ))}
-        </ul>
+                {selectedContact === contact.id && (
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      value={note}
+                      onChange={(e) => setNote(e.target.value)}
+                      className="w-full px-3 py-2 border rounded mb-2"
+                      placeholder="Enter note"
+                    />
+                    <button
+                      onClick={() => handleAddNote(contact.id)}
+                      className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
+                    >
+                      Save Note
+                    </button>
+                  </div>
+                )}
+                {contact.notes && (
+                  <p className="mt-2 text-gray-600">{contact.notes}</p>
+                )}
+              </li>
+            ))}
+          </ul>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-1 rounded text-center"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? "Hide All Contacts" : "Show All Contacts"}
+          </button>
+          {open && (
+            <ul>
+              {updatedContacts.map((contact) => (
+                <li key={contact.id} className="border-b py-2">
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <p>
+                        <b>{contact.name}</b>
+                      </p>
+                      <p>{contact.phone_number}</p>
+                    </div>
+                    <button
+                      onClick={() => setSelectedContact(contact.id)}
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                    >
+                      Add Note
+                    </button>
+                  </div>
+                  {selectedContact === contact.id && (
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        value={note}
+                        onChange={(e) => setNote(e.target.value)}
+                        className="w-full px-3 py-2 border rounded mb-2"
+                        placeholder="Enter note"
+                      />
+                      <button
+                        onClick={() => handleAddNote(contact.id)}
+                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
+                      >
+                        Save Note
+                      </button>
+                    </div>
+                  )}
+                  {contact.notes && (
+                    <p className="mt-2 text-gray-600">{contact.notes}</p>
+                  )}
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
       )}
     </div>
   );
